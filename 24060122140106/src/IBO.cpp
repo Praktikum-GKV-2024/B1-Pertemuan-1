e8e617ba748c92a742a7bbaf5dd4bbcfff7625d5
ChandraWijayakusuma <chandra.wijayakusuma88@gmail.com>
2024-03-05 18:43:18 +0700

    kumpul tugas

--- 24060122140106/src/IBO.cpp
+++ 24060122140106/src/IBO.cpp
@@ -3,18 +3,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <glm/vec3.hpp> // glm::vec3
-#include <glm/vec4.hpp> // glm::vec4
-#include <glm/mat4x4.hpp> // glm::mat4
-#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
-#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
-#include <glm/ext/scalar_constants.hpp> // glm::pi
+#include <glm/vec3.hpp>
 #include <iostream>
-#include <cmath>
-#include <vector>
-#include <cstring>
-#include <common/loader.hpp>
-#include <common/controls.hpp>
 #include <common/shader.hpp>
 
 #define GLCall(x) GLClearError(); x; GLLogCall(#x, __FILE__, __LINE__)
@@ -22,9 +12,10 @@
 static void GLClearError() {
     while (glGetError() != GL_NO_ERROR);
 }
+
 static void GLLogCall(const char* function_name, const char* file, int line) {
     while (GLenum error = glGetError()) {
-        cout << "Triggered Error Code: " << error << " at function call " << function_name << ", at " << file << ":" << line << std::endl;
+        std::cout << "Triggered Error Code: " << error << " at function call " << function_name << ", at " << file << ":" << line << std::endl;
     }
 }
 
@@ -36,15 +27,15 @@ public:
     GLuint ibo;
     GLuint programId;
 
-    MainScene (GLFWwindow* window) {
+    MainScene(GLFWwindow* window) {
         this->window = window;
 
-        glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+        glClearColor(0.0f, 0.0f, 0.5f, 1.0f);
 
         // Enable depth test
         glEnable(GL_DEPTH_TEST);
         // Accept fragment if it is closer to the camera than the former one
-        glDepthFunc(GL_LESS); 
+        glDepthFunc(GL_LESS);
 
         // Cull triangles which normal is not towards the camera
         // glEnable(GL_CULL_FACE);
@@ -55,17 +46,47 @@ public:
     void start() {
         programId = LoadShaders("res/shader/super_basic.vs", "res/shader/super_basic.fs");
 
-        // vertecies yang di pass ke GPU
+        // vertices to pass to GPU
         float positions[] = {
-             0.5f,  0.5f, // 0
-             0.5f, -0.5f, // 1
-            -0.5f, -0.5f, // 2
-            -0.5f,  0.5f  // 3
+            0.f,  0.f,     1.0, 1.0, 1.0, // T0 - Putih
+            0.3f, 0.f,     1.0, 0.0, 0.0, // T1 - Merah
+            0.f, -0.3f,    1.0, 0.0, 0.0, // T2 - Merah
+            -0.3f, 0.f,    1.0, 0.0, 0.0, // T3 - Merah
+            0.f, 0.3f,     1.0, 0.0, 0.0, // T4 - Merah
+            0.5f, 0.f,     0.0, 1.0, 0.0, // T5 - Hijau
+            0.f, -0.5f,    0.0, 1.0, 0.0, // T6 - Hijau
+            -0.5f, 0.f,    0.0, 1.0, 0.0, // T7 - Hijau
+            0.f, 0.5f,     0.0, 1.0, 0.0, // T8 - Hijau
+            0.8f, 0.f,     0.0, 0.0, 1.0, // T9 - Biru
+            0.f, -0.8f,    0.0, 0.0, 1.0, // T10 - Biru
+            -0.8f, 0.f,    0.0, 0.0, 1.0, // T11 - Biru
+            0.f, 0.8f,     0.0, 0.0, 1.0  // T12 - Biru
         };
+
+
 
         unsigned int indices[] = {
             0, 1, 2,
-            2, 3, 0
+            0, 2, 3,
+            0, 3, 4,
+            0, 1, 4,
+            1, 5, 8,
+            1, 5, 2,
+            2, 5, 6,
+            2, 6, 3,
+            3, 7, 6,
+            3, 7, 4,
+            4, 7, 8,
+            1, 4, 8,
+            5, 12, 9,
+            5, 9, 6,
+            6, 9, 10,
+            6, 7, 10,
+            7, 10, 11,
+            7, 8, 11,
+            8, 11,12,
+            8, 5, 12
+
         };
 
         // Initialize Vertex Array Buffer
@@ -75,42 +96,51 @@ public:
         // setup vertex buffers
         glGenBuffers(1, &buffer);
         glBindBuffer(GL_ARRAY_BUFFER, buffer);
-        glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, 5 * 13 * sizeof(float), positions, GL_STATIC_DRAW);
 
         // setting the layout
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(
-            0, // index untuk layout VAO
-            2, // vector size of data type
-            GL_FLOAT, // data type
-            GL_FALSE, // normalized? map to 0 - 255
-            2 * sizeof(float), // gaps
-            0                  // offset
+            0,                  // index for layout VAO
+            2,                  // vector size of data type
+            GL_FLOAT,           // data type
+            GL_FALSE,           // normalized? map to 0 - 255
+            5 * sizeof(float),  // gaps
+            0                   // offset
         );
+
+
+        glEnableVertexAttribArray(1);
+                glVertexAttribPointer(
+                    1, // index untuk layout VAO
+                    3, // vector size of data type
+                    GL_FLOAT, // data type
+                    GL_FALSE, // normalized?
+                    5 * sizeof(float), // gaps
+                    (void*)(2* sizeof(float)) // offset
+                );
+
+
 
         glGenBuffers(1, &ibo);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * 2 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, 20 * 3 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
         glBindVertexArray(0);
         glUseProgram(0);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
     }
 
     void update() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        /* do every frame here*/
+        // do every frame here
         glUseProgram(programId);
 
         glBindVertexArray(vao);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-
-
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 20 * 3, GL_UNSIGNED_INT, nullptr);
     }
 };
